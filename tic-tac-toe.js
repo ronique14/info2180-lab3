9ceddade0613ceaf4f74b7fc4236ec42752a54f9
Ronique Reid <roni.reid.roni@gmail.com>
2020-10-10 18:17:48 -0500

    Add an X and O to square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,33 @@
 // JavaScript source code
 window.onload = function () {
-    let square = document.querySelectorAll("#board div");
+    const square = document.querySelectorAll("#board div");
+   
+    let currentPlayer = 'X';
     for (let i = 0; i < square.length; i++) {
         square[i].classList.add('square');
     }
     console.log(square);
+
+
+    square.forEach(square => {
+        square.addEventListener('click', clickOutcome, { once: true })
+       
+    })
+
+
+function clickOutcome(e) {
+   const squareArray = Array.from(square);
+    const index = squareArray.indexOf(e.target);
+    square[index].innerHTML = currentPlayer;
+    
+
+    if (currentPlayer == 'X') {
+        square[index].classList.add('X');
+        currentPlayer = 'O';
+    } else {
+     square[index].classList.add('O');
+    currentPlayer = 'X'}
+    console.log(index);
+    
+ }
 }
