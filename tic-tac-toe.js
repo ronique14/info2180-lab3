94f62fcb5729511ccf9b5c6de202ea48e455bcbf
Ronique Reid <roni.reid.roni@gmail.com>
2020-10-11 15:37:17 -0500

    Hover for the squares

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,33 +1,63 @@
 // JavaScript source code
 window.onload = function () {
     const square = document.querySelectorAll("#board div");
-   
+
+    
+
     let currentPlayer = 'X';
     for (let i = 0; i < square.length; i++) {
         square[i].classList.add('square');
+
+        for (let i = 0; i < square.length; i++) {
+            square[i].addEventListener('mouseover', mouseOver)
+            square[i].addEventListener('mouseout', mouseOut)
+
+
+            function mouseOver() {
+                square[i].classList.add('hover');
+            }
+
+            function mouseOut() {
+                square[i].classList.remove('hover');
+            }
+
+            
+        }
     }
     console.log(square);
 
 
+
+
     square.forEach(square => {
         square.addEventListener('click', clickOutcome, { once: true })
-       
     })
 
 
-function clickOutcome(e) {
-   const squareArray = Array.from(square);
-    const index = squareArray.indexOf(e.target);
-    square[index].innerHTML = currentPlayer;
-    
 
-    if (currentPlayer == 'X') {
-        square[index].classList.add('X');
-        currentPlayer = 'O';
-    } else {
-     square[index].classList.add('O');
-    currentPlayer = 'X'}
-    console.log(index);
-    
- }
-}
+    function clickOutcome(e) {
+        const squareArray = Array.from(square);
+        const index = squareArray.indexOf(e.target);
+        square[index].textContent = currentPlayer ;
+
+
+        if (currentPlayer == 'X') {
+            square[index].classList.add('X');
+            currentPlayer = 'O';
+        } else {
+            square[index].classList.add('O');
+            currentPlayer = 'X'
+        }
+        console.log(square[index]);
+
+    }
+
+
+
+
+
+
+
+
+    }
+ 
