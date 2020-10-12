2ddb2339cf593d1ef9c83750d546683f71c49652
Ronique Reid <roni.reid.roni@gmail.com>
2020-10-11 23:06:38 -0500

    Restart game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,15 +1,15 @@
 // JavaScript source code
 window.onload = function () {
     const square = document.querySelectorAll("#board div");
-    const winningMessage = () => "Congratulations! ${currentPlayer} is the winner!";
-    const drawMessage = () => "Game ended in a draw!";
+    const winningMessage = () => document.querySelector("#status div") 
+    const winningMessageElement = document.getElementById("winningMessage");
+    const newGameButton = document.getElementById("btn");
+   
 
 
-    let message = document.getElementById("#status");
+    //let message = document.getElementById("#status");
 
     let currentPlayer = 'X';
-    let gameState = ["", "", "", "", "", "", "", "", ""];
-
 
     for (let i = 0; i < square.length; i++) {
         square[i].classList.add('square');
@@ -59,11 +59,16 @@ window.onload = function () {
 
     }
 
-    
+    if (checkWin()) {
+        endGame(false)
+    } else (isDraw())
+    {
+        endGame(true)
+    }
 
     this.positions = Array.from(square);
 
-    this.checkWin = function () {
+     function checkWin () {
         let winner = false;
         const winningCombo = [
             [0, 1, 2],
@@ -102,6 +107,41 @@ window.onload = function () {
 
     }
 
-}
-  
+    function endGame(draw) {
+        if (draw) {
+            winningMessageTextElement.innerText = 'Draw!'
+        } else {
+            winningMessageTextElement.innerText = $(currentplayer ? "Congratulations! X is the Winner!" : "Congratulations! O is the Winner!")
+        }
+        winningMessageElement.classList.add('show')
+    }
 
+    function isDraw() {
+        return square.every(square => {
+            return square.classList.contains('X') ||
+                square.classList.contains('O');
+
+
+        })
+    }
+
+
+    newGameButton.addEventListener('click', reset)
+
+    function reset(){
+        square.forEach(square => {
+            square.classList.remove('X')
+            square.classList.remove('O')
+            square.removeEventListener('click', clickOutcome)
+            winnigMessageElement.classList.remove('show')
+        })
+    }
+}
+
+
+
+
+
+
+
+     
